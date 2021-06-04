825f1ad171bef8a95f6538416f51f3d5a111407d
saidatunnizmah <aniskateha@gmail.com>
2021-06-04 21:26:06 +0800

    penambahan halaman about

---
+++ about_us.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>About us</title>
+    <link rel="stylesheet" type="text/css" href="tentang.css">
+    <div class="topnav">
+        <a href="HOMEFIX.php">Home</a>
+        <a href="KATEGORI.php">Kategori</a>
+        <a href="KONSULTASI.php">Konsultasi</a>
+        <a style="background-color:#ddd;color:black" href="#">Tentang</a>
+    </div>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous">
+    <style>
+
+
+    </style>
+</head>
+
+<body>
+    <h1>About Us</h1><br><br><br><br><br>
+    <div style="display: inline;"><img src="bunga.jpg" style="margin-top: 10px;width: 49.5%;height: 50%;"></div>
+    <p class="sedang" style="display: inline;margin-top: 10px;margin-left: 20px;">Gaptek.id merupakan web penyedia informasi tantang spesifikasi dan harga berbagai jenis laptop.
+        Untuk menjalankan prosesnya, ada beberapa fitur yang terdapat pada website diantaranya, home,
+        kategori produk, konsultasi, contact person, alamat. Permasalahannya yaitu tidak semua orang mengetahui
+        dan mengerti kesesuaian tipe laptop dengan kebutuhan dan budget pengguna.</p>
+    <!-- <table width="100%" border="1">
+        <tr>
+            <td><img src="fahmi.jpg" alt="" style="width: 400px;height: 300px;"></td>
+            <td><img src="zulfian.jpg" alt="" style="width: 400px;height: 300px;"></td>
+            <td><img src="saidatun.jpg" alt="" style="width: 400px;height: 300px;"></td>
+        </tr>
+    </table> -->
+    <h1 style="display:inline">Our Team</h1>
+    <div style="display: inline-block; height:45%; width: 100%;">
+    <div style="display: inline;"><img src="fahmi.jpg" style="width: 20%;height: 45%;margin-top: 4px;"></div>
+    <div style="display: inline;"><img src="zulfian.jpg" style="margin-left: 330px;width: 20%;height: 45%;"></div>
+    <div style="display: inline;"><img src="saidatun.jpg" style="margin-left: 330px;width: 20%;height: 45%;"></div>
+    </div>
+    <!-- <div style="display: inline; "><img src="bunga.jpg" style="width: 24.5%;height: 25%;margin-top: 4px;"></div>
+        <div style="display: inline;"><img src="bunga.jpg" style="margin-left: 4px;width: 24.5%;height: 25%;"></div>
+        <div style="display: inline;"><img src="bunga.jpg" style="margin-right: 4px;width: 24.5%;height: 25%;"></div> -->
+</body>
+<footer class="footer">
+    <div style="text-align: center;display: inline-block;">
+        <i class="fas fa-map-marker-alt"></i><br>
+        - Balangan, Kalimantan Selatan<br>
+        - Barabai, Kalimantan Selatan<br>
+        - Banjarbaru, Kalimantan Selatan</p>
+    </div>
+    <div style="text-align: center;display: inline-block;margin-left:364px">
+        <i class="fas fa-users"></i><br>
+        <a style="text-decoration:none;" class="mbf" href="https://www.instagram.com/m.b_fahmi/">- Muhammad Bahrul Fahmi</a><br>
+        <a style="text-decoration:none;" class="sn" href="https://www.instagram.com/anis.incun/">- Saidatun Nizmah</a><br>
+        <a style="text-decoration:none;" class="zyr" href="https://www.instagram.com/zulfian.yr/">- Zulfian Yafi Rizani</a></p>
+    </div>
+    <div style="text-align: center;display: inline-block;margin-left:364px"><br>
+        <i class="fas fa-envelope"></i><br>
+        - bahrulfahmi591@gmail.com<br>
+        - annizmah14@gmail.com<br>
+        - zulfianyr9@gmail.com</p>
+    </div>
+</footer>
+<footer class="superfooter">
+    <i class="fab fa-instagram"></i>
+    <i class="fab fa-facebook-square" style="display: inline;"></i>
+    <i class="fas fa-globe" style="display: inline;float: right;"> Indonesia</i>
+    <p style="display: inline;">&copy;Copyrigt 2021</p>
+</footer>
+
+</html>
