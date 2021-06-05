62c6a7a96c08d4dfb4e5ece777d6f27989c22639
BahrulFahmi <bahrulfahmi591@gmail.com>
2021-06-05 18:40:20 +0700

    perbaikan css

--- asus.php
+++ asus.php
@@ -89,6 +89,7 @@
 	            		</div>	
 	            </div>
             <a href="KONSULTASI2.php">Konsultasi</a>
+			<h2 style="margin-top:310px; "><a class="aw" href="tambah.php" class="btn btn-sm btn-success right">Tambah Kategori</a></h2>
          </div>
          <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
          integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
@@ -100,9 +101,6 @@
         <?php
          require 'function.php';
          $data = query("SELECT * FROM kategori WHERE merk='Asus'");
-         
-         // $img = ['','1.jpg', '2.jpg' ,'3.jpg', '4.png','5.png', '6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.png','13.jpg','14.jpg'];
-
          ?>
       
 
@@ -122,7 +120,7 @@
 	<?php $i++; ?>	
 	<?php endforeach ?>
 	
-	<h2 style="margin-top:310px; "><a class="aw" href="tambah.php" class="btn btn-sm btn-success right">Tambah Kategori</a></h2>
+	
 	
 	<?php
 	$i=1; 
