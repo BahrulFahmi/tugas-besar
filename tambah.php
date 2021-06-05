645dbc1c47dadae338a89f922db46278d9d2067d
saidatunnizmah <aniskateha@gmail.com>
2021-06-05 18:10:39 +0800

    css tambah

--- tambah.php
+++ tambah.php
@@ -1,74 +1,72 @@
-<?php 
+<?php
 require 'function.php';
-if(isset($_POST["submit"])){
-	
-if (tambah($_POST) > 0){
-	echo "
+if (isset($_POST["submit"])) {
+
+	if (tambah($_POST) > 0) {
+		echo "
 	<script>
 		alert ('Produk Berhasil Ditambahkan');
 		document.location.href='KATEGORI2.php';
 	</script>
 	";
-}else{
-echo "
+	} else {
+		echo "
 	<script>
 		alert ('Produk Gagal Ditambahkan');
 		document.location.href='tambah.php';
 	</script>
 	";
+	}
 }
-
-
-
-}
- ?>
+?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>Tambah Produk</title>
-	<link rel="stylesheet" type="text/css" href="tambah.css">		
+	<link rel="stylesheet" type="text/css" href="tambah.css">
 </head>
+
 <body>
 
 
-	<div >
+	<div class="tambah">
 
-		
+
 		<form action="" method="post" enctype="multipart/form-data">
-			
-			<ul >
-				<li ><h1>Tambah Produk</h1></li>
-				<li >
+			<h1>Tambah Produk</h1>
+			<div>
+
+
+				<div>
 					<input type="text" name="judul" id="judul" placeholder="Judul" required>
-				</li>
-				<li >
+				</div>
+				<div>
 					<select name="merk" required="">
 						<option value="Acer">Acer</option>
 						<option value="Asus">Asus</option>
 						<option value="HP">HP</option>
 						<option value="Lenovo">Lenovo</option>
 					</select>
-				</li>
-				<li >
+				</div>
+				<div>
 					<input type="text" name="harga" id="harga" placeholder="harga" required>
-				</li>
-				<!-- <li >
-					<input type="text" name="spek" id="spek" placeholder="spek" required>
-				</li> -->
-				<li>
+				</div>
+				<div>
 					<textarea rows="3" placeholder="Spek" name="spek"></textarea>
-				</li>
-				<li>
-				<input type="file" name="img" id="img" >
-				</li>
-				<li ><button type="submit" name="submit">Tambah</button></li>
+				</div>
+				<div>
+					<input type="file" name="img" id="img">
+				</div>
+				<div><button type="submit" name="submit">Tambah</button></div>
 
 
-			</ul>
+			</div>
 
 		</form>
 	</div>
 
 </body>
+
 </html>
